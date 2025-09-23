f05a10a94e0c66312bb20f801655ab8307c94f8c
Lucas Monteiro <lhmonteiro.ti@gmail.com>
2025-09-23 16:39:11 -0300

    Delete backend/database/Backup directory

--- backend/database/Backup/backup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set data=%date:~6,4%-%date:~3,2%-%date:~0,2%
-set hora=%time:~0,2%-%time:~3,2%
-set backup_dir=C:\RX-system\backend\database\Backup
-set filename=backup_%data%_%hora%.sql
-
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" -u mestre -p123456 bdraiox > "%backup_dir%\%filename%"
